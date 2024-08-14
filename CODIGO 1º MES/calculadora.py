3f454027e9e8dc09676d86d8492f87a3a5e98cc5
NataliaUrzagaste <natalia.urzagaste@ucb.edu.bo>
2024-08-14 13:45:44 -0900

    v1

---
+++ CODIGO 1º MES/calculadora.py
@@ -0,0 +1,9 @@
+x=int(input("dame el primer numero")) #el int es para numeros, el input en general todo(usa mas espacio de memoria)
+y=int(input("dame el segundo numero"))
+print(x) #puedes poner el int a la hora de imprimir para definir la variable tambien
+print(type(x)) # el type es para definir la variable de retorno XD
+suma=x+y
+print(suma)
+resta=x-y
+print(resta)
+
