9a7533ab221dfbdb9c05217a2441eb07159cce41
NataliaUrzagaste <natalia.urzagaste@ucb.edu.bo>
2024-08-16 14:29:21 -0900

    stdin

--- CODIGO 1º MES/hackerRank.py
+++ CODIGO 1º MES/hackerRank.py
@@ -19,3 +19,13 @@ if __name__ == '__main__':
         print("Weird")
     else:
         print("Not Weird")
+
+
+#stdin
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a + b)
+    print(a - b)
+    print(a * b)
+    
