cc6344795b7184cf90a9730022cad4edd4907725
NataliaUrzagaste <natalia.urzagaste@ucb.edu.bo>
2024-08-21 13:24:05 -0900

    v4

--- CODIGO 1º MES/hackerRank.py
+++ CODIGO 1º MES/hackerRank.py
@@ -32,3 +32,11 @@ if __name__ == '__main__':
     print(a - b)
     print(a * b)
     
+#division
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    div= a//b
+    div2= a/b
+    print(div)
+    print(div2)
