75942fca91e7514f3ed583d8dc2fbec9b5b9e580
NataliaUrzagaste <natalia.urzagaste@ucb.edu.bo>
2024-08-28 13:06:19 -0900

    v6

--- CODIGO 1º MES/hackerRank.py
+++ CODIGO 1º MES/hackerRank.py
@@ -68,3 +68,9 @@ def is_leap(year):
 
 year = int(input())
 print(is_leap(year))
+
+#metodods
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1, n+1):
+        print(i, end="")
