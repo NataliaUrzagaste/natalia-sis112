909739ff1c000d625fa5cbc199e1a6afef44264c
NataliaUrzagaste <natalia.urzagaste@ucb.edu.bo>
2024-08-30 14:21:57 -0900

    v1

---
+++ CODIGO 1º MES/listas y matrices.py
@@ -0,0 +1,10 @@
+#listas y matrices
+# hacer el promedio de las notas con matrices
+n=int(input("estudiantes"))
+x=int(input("nota"))
+c=int(input("nota"))
+v=int(input("nota"))
+b=int(input("nota"))
+notas=[x,c],
+[v,b]
+print(x+c+v+b/n)
